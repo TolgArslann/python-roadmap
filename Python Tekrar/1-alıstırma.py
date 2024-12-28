f19b0241b647f7d1013b7dc3de32e0c12420ee23
TolgArslann <t0lg4.urun@gmail.com>
2024-12-29 00:53:35 +0300

    asd

--- Python Tekrar/1-alıstırma.py
+++ Python Tekrar/1-alıstırma.py
@@ -3,3 +3,5 @@ user_stok = int(input("Stok miktarini giriniz : "))
 user_fiyat = float(input("Urun Fiyatini giriniz :"))
 
 print(f"Urun ismi : {user_urun}\nStok Miktari : {user_stok}\nUrun Fiyati : {user_fiyat} ")
+
+print()
