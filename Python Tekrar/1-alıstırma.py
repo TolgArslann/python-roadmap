98480b0788e11ca3761dc37a78049c6aaea17ad4
TolgArslann <t0lg4.urun@gmail.com>
2024-12-29 00:26:38 +0300

    Python Tekrar klasörü ve dosyaları eklendi

--- Python Tekrar/1-alıstırma.py
+++ Python Tekrar/1-alıstırma.py
@@ -1,1 +1,5 @@
-print('Merhaba Dünya')
+user_urun = input("Urun ismini giriniz : ")
+user_stok = int(input("Stok miktarini giriniz : "))
+user_fiyat = float(input("Urun Fiyatini giriniz :"))
+
+print(f"Urun ismi : {user_urun}\nStok Miktari : {user_stok}\nUrun Fiyati : {user_fiyat} ")
