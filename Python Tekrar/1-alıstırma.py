dacb364c047cb933d5bd889561a30ca7a310fb13
TolgArslann <t0lg4.urun@gmail.com>
2024-12-29 14:43:28 +0300

    Ilk Kisim

--- Python Tekrar/1-alıstırma.py
+++
@@ -1,7 +0,0 @@
-user_urun = input("Urun ismini giriniz : ")
-user_stok = int(input("Stok miktarini giriniz : "))
-user_fiyat = float(input("Urun Fiyatini giriniz :"))
-
-print(f"Urun ismi : {user_urun}\nStok Miktari : {user_stok}\nUrun Fiyati : {user_fiyat} ")
-
-print('')
---
+++ Python Tekrar/1-kontrolYapılari.py
@@ -0,0 +1,83 @@
+
+
+def pozNeg():
+    """
+    1. Koşullu İfadeler (if-elif-else)
+    Kolay:
+    Kullanıcıdan bir sayı al ve bu sayının pozitif mi, negatif mi yoksa sıfır mı olduğunu kontrol et.
+    """
+
+    user_num = int(input("Bir sayi giriniz : "))
+    if user_num > 0:
+        print("Pozitif")
+    elif user_num < 0:
+        print("Negatif")
+    else:
+        print("Notr")
+
+def ageCont():
+    """
+    Orta:
+    Kullanıcıdan yaşını al ve yaşına göre kategori belirle:
+
+    0-12: Çocuk
+    13-19: Genç
+    20-64: Yetişkin
+    65 ve üzeri: Yaşlı
+    """
+    user_age = int(input("Lutfen yasinizi giriniz : "))
+
+    if user_age > 64:
+        print("Yasli")
+    elif user_age > 19:
+        print("Yetiskin")
+    elif user_age > 12:
+        print("Genc")
+    elif user_age >= 0:
+        print("Cocuk")
+    else:
+        print("Gecerli bir deger giriniz...")
+
+def pswCont():
+    """
+    Orta Zor:
+    Kullanıcıdan bir şifre al ve şu kurallara uyup uymadığını kontrol et:
+
+    Şifre en az 8 karakter uzunluğunda olmalı.
+    En az bir büyük harf ve bir küçük harf içermeli.
+    En az bir sayı içermeli.
+    """
+    user_psw = input("Lutfen bir sifre giriniz : ")
+    if user_psw.isupper() and user_psw.islower():
+        print("Girdiginiz sifre gecerli")
+    else:
+        print("Girdiginiz sifre en az 8 karakter ve buyuk kucuk harf icermelidir")
+
+
+def twoNum():
+    """
+    Zor:
+    Kullanıcıdan aldığı iki sayıyı karşılaştıran bir program yaz:
+
+    Eğer iki sayı eşitse, ekrana "Eşit" yaz.
+    Eğer birinci sayı büyükse, farkını yaz.
+    Eğer ikinci sayı büyükse, oranlarını yaz
+    """
+    user_num1 = int(input("Lutfen ilk sayiyi giriniz :"))
+    user_num2 = int(input("Lutfen ikinci sayiyi giriniz :"))
+
+    if user_num1 == user_num2:
+        print("Girdiginiz iki deger birbirine esitler")
+    elif user_num1 > user_num2:
+        new_num = user_num1 - user_num2
+        print(f"\nGirilen sayilar:\n"
+            f"Sayi 1 = {user_num1}\n"
+            f"Sayi 2 = {user_num2}\n"
+            f"Sayilarin farki = {new_num}")
+    elif user_num1 < user_num2:
+        new_or = user_num2/user_num1
+        print(f"\nGirilen sayilar:\n"
+            f"Sayi 1 = {user_num1}\n"
+            f"Sayi 2 = {user_num2}\n"
+            f"Sayilarin Orani = {new_or}")
+
