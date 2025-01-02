253160c72619da6a00a32d411e69725f029cab23
TolgArslann <t0lg4.urun@gmail.com>
2025-01-03 01:26:55 +0300

    up1

--- 4-fonksiyonlar.py
+++ 4-fonksiyonlar.py
@@ -4,19 +4,20 @@
 # Sıfıra bölme hatasını (ZeroDivisionError) kontrol edin.
 # Yanlış türde veri girilirse (ValueError) kullanıcıya açıklayıcı mesaj verin.
 def divisionNums():
-    num1 = float(input("Bolunecek sayiyi giriniz :"))
-    num2 = float(input("Bolecek sayiyi giriniz :"))    
+    num1 = input("Bolunecek sayiyi giriniz :")
+    num2 = input("Bolecek sayiyi giriniz :")  
     try:
-        value1= num1
-        value2= num2
+        value1= float(num1)
+        value2= float(num2)
+        sonuc = value1 / value2
     except ValueError:
         print("Lutfen sadece sayi giriniz....")
-        return
+        return divisionNums()
     except ZeroDivisionError:
         print("Bolen sayi sifir olamaz..Lutfen tekrar bir deger giriniz...")
-        return
-    else:
-        sonuc = num1 / num2                       
+        sonuc=None
+        return divisionNums()
+    else:                      
         print(f"{num1}/{num2} = {sonuc}")
 divisionNums()
 
